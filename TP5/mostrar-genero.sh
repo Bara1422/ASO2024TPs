9ea1071e54fb4e697b503848abfbc062fc9f38af
bara1422 <xxbaraxx@hotmail.com>
2024-06-10 23:16:26 -0300

    fix respuesta vacia en mostrar-genero

--- TP5/mostrar-genero.sh
+++ TP5/mostrar-genero.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-
 echo "Mostrar genero nombre ingresado por usuario"
+echo "Para correr este script primero hay que tener instalado jq"
+echo "Queres instalarlo?"
+read respuesta
+while [[ $respuesta != "s" && $respuesta != "S" && $respuesta != "n" && $respuesta != "N" ]];
+    do
+    case $respuesta in 
+        "S"|"s")
+        echo "Se va a instalar jq"
+        sudo apt install jq
+    ;;
+        "N"|"n")
+        echo "No se va a instalar jq"
+    ;;
+        *)
+        echo "Por favor escriba s/n"
+        read respuesta
+    ;;
+        esac
+done
 
 echo "Por favor ingrese el nombre del que quiere saber el género"
 read nombreIngresado
+while [[ $nombreIngresado == "" ]];
+    do
+    echo "Por favor ingrese un nombre valido"
+    read nombreIngresado
+done
 
 API="https://api.genderize.io/?name=$nombreIngresado"
 
